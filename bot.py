43e017b5b6aedaaa1a1c4b4f479750445639357c
AleksandrB00 <babaev_aleksandr@icloud.com>
2022-09-01 16:49:48 +0200

    first commit

---
+++ bot.py
@@ -0,0 +1,77 @@
+import logging
+import config
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+from aiogram.dispatcher.filters.state import State, StatesGroup
+from aiogram import Bot, Dispatcher, types, executor
+import request
+from aiogram.dispatcher import FSMContext
+
+logging.basicConfig(level=logging.INFO)
+token = Bot(token=config.bot_token)
+storage = MemoryStorage()
+bot = Dispatcher(token, storage=storage)
+
+class ChoiceCity(StatesGroup):
+    waiting_city = State()
+
+
+@bot.message_handler(commands=['start'])
+async def start_message(message):
+    markup = types.reply_keyboard.ReplyKeyboardMarkup(row_width=2)
+    btn1 = types.KeyboardButton('Погода в моём городе')
+    btn2 = types.KeyboardButton('Погода в другом месте')
+    btn3 = types.KeyboardButton('История')
+    markup.add(btn1, btn2, btn3)
+    text = f'Привет @{message.from_user.username}, я бот, который расскжет тебе о погоде на сегодня'
+    await message.answer(text, reply_markup=markup)
+
+@bot.message_handler(regexp='Погода в моём городе')
+async def wallet(message):
+    markup = types.reply_keyboard.ReplyKeyboardMarkup(row_width=2, resize_keyboard=True)
+    btn1 = types.KeyboardButton('Меню')   
+    markup.add(btn1)  
+    data = request.get_weather()
+    text = f' Температура: {data["temp"]} C\n Ощущается как: {data["feels_like"]} C \n Скорость ветра: {data["wind_speed"]}м/с\n Давление: {data["pressure_mm"]}мм'
+    await message.answer(text, reply_markup=markup)
+
+@bot.message_handler(regexp='Меню')
+async def menu(message):
+    markup = types.reply_keyboard.ReplyKeyboardMarkup(row_width=2)
+    btn1 = types.KeyboardButton('Погода в моём городе')
+    btn2 = types.KeyboardButton('Погода в другом месте')
+    btn3 = types.KeyboardButton('История')
+    markup.add(btn1, btn2, btn3)
+    text = f'Привет @{message.from_user.username}, я бот, который расскжет тебе о погоде на сегодня'
+    await message.answer(text, reply_markup=markup)
+
+def register_handlers_city(dp: Dispatcher):
+    dp.register_message_handler(city_start, regexp="Погода в другом месте", state="*")
+    dp.register_message_handler(city_chosen, state=ChoiceCity.waiting_city)
+
+@bot.message_handler(regexp='Погода в другом месте', state="*")
+async def city_start(message):
+    markup = types.reply_keyboard.ReplyKeyboardMarkup(row_width=2, resize_keyboard=True)
+    btn1 = types.KeyboardButton('Меню')
+    markup.add(btn1)
+    text = 'Введите название города'
+    await message.answer(text, reply_markup=markup)
+    await ChoiceCity.waiting_city.set()
+
+@bot.message_handler(state=ChoiceCity.waiting_city)
+async def city_chosen(message: types.Message, state: FSMContext):
+    if message.text[0].islower():
+        await message.answer('Названия городов пишутся с большой буквы)')
+        return
+    await state.update_data(waiting_city=message.text.lower())
+    markup = types.reply_keyboard.ReplyKeyboardMarkup(row_width=2, resize_keyboard=True)
+    btn1 = types.KeyboardButton('Меню')
+    markup.add(btn1)
+    user_data = await state.get_data()
+    data = request.get_weather_by_city(user_data.get('waiting_city'))
+    text = f' Температура: {data["temp"]} C\n Ощущается как: {data["feels_like"]} C \n Скорость ветра: {data["wind_speed"]}м/с\n Давление: {data["pressure_mm"]}мм'
+    await message.answer(text, reply_markup=markup)
+
+
+
+if __name__ == '__main__':
+    executor.start_polling(bot, skip_updates=True)
---
+++ request.py
@@ -0,0 +1,23 @@
+import requests
+import config
+import json
+
+def get_weather():
+    r = requests.get('https://api.weather.yandex.ru/v2/forecast', params=config.payload_weather, headers=config.weather_key)
+    weather_data = r.json()['fact']
+    return weather_data
+
+def get_city_coord(city):
+    payload = {'geocode' : city, 'apikey' : config.geo_key, 'format' : 'json'}
+    r = requests.get('https://geocode-maps.yandex.ru/1.x', params=payload)
+    geo = json.loads(r.text)
+    return geo['response']['GeoObjectCollection']['featureMember'][0]['GeoObject']['Point']['pos'].split()
+
+def get_weather_by_city(city):
+    coordinates = get_city_coord(city)
+    r = requests.get('https://api.weather.yandex.ru/v2/forecast', params={'lat': coordinates[1], 'lon': coordinates[0], 'lang': 'ru_RU', 'limit': '1'}, headers=config.weather_key)
+    weather_data = r.json()['fact']
+    return weather_data
+
+
+
